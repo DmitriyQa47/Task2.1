73450d0e2bdccc001cfeda7ce1a14125ca7b9b30
Dmitriy Sikorskiy <sikorskiy_1997@list.ru>
2023-03-20 15:17:33 +0300

    ititial commit

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest1.java
@@ -0,0 +1,24 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .header("Content-Type", "application/json; charset=utf-8")
+                .contentType(ContentType.JSON)
+        ;
+    }
+}
